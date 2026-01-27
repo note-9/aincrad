01f186f05f82c96a4528d98e90b732ccd35a032f
note-9 <heemanshbhawsar2004@gmail.com>
2026-01-27 17:27:40 +0530

    shifted most of the work to the gpu with tessalation shaders

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,7 @@ void processInput(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 800;
+const unsigned int NUM_PATCH_PTS = 4;
 int useWireframe = 0;
 int displayGrayscale = 0;
 // Camera state
@@ -42,21 +43,106 @@ bool firstMouse = true;
 // Zoom
 float fov = 45.0f;
 
-const char* VS = R"(
+const char* TCS =R"(
 #version 450 core
-layout (location=0) in vec3 aPos;
+layout (vertices=4) out;
 
+uniform mat4 model;
+uniform mat4 view;
+
+in vec2 TexCoord[];
+out vec2 TextureCoord[];
+
+void main(){
+  gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+  TextureCoord[gl_InvocationID] = TexCoord[gl_InvocationID];
+
+  if (gl_InvocationID == 0) {
+    const int MIN_TESS_LEVEL = 4;
+    const int MAX_TESS_LEVEL = 64;
+    const float MIN_DISTANCE = 20;
+    const float MAX_DISTANCE = 800;
+    vec4 eyeSpacePos00 = view * model * gl_in[0].gl_Position;
+    vec4 eyeSpacePos01 = view * model * gl_in[1].gl_Position;
+    vec4 eyeSpacePos10 = view * model * gl_in[2].gl_Position;
+    vec4 eyeSpacePos11 = view * model * gl_in[3].gl_Position;
+
+    // "distance" from camera scaled between 0 and 1
+    float distance00 = clamp( (abs(eyeSpacePos00.z) - MIN_DISTANCE) / (MAX_DISTANCE-MIN_DISTANCE), 0.0, 1.0 );
+    float distance01 = clamp( (abs(eyeSpacePos01.z) - MIN_DISTANCE) / (MAX_DISTANCE-MIN_DISTANCE), 0.0, 1.0 );
+    float distance10 = clamp( (abs(eyeSpacePos10.z) - MIN_DISTANCE) / (MAX_DISTANCE-MIN_DISTANCE), 0.0, 1.0 );
+    float distance11 = clamp( (abs(eyeSpacePos11.z) - MIN_DISTANCE) / (MAX_DISTANCE-MIN_DISTANCE), 0.0, 1.0 );
+
+    float tessLevel0 = mix( MAX_TESS_LEVEL, MIN_TESS_LEVEL, min(distance10, distance00) );
+    float tessLevel1 = mix( MAX_TESS_LEVEL, MIN_TESS_LEVEL, min(distance00, distance01) );
+    float tessLevel2 = mix( MAX_TESS_LEVEL, MIN_TESS_LEVEL, min(distance01, distance11) );
+    float tessLevel3 = mix( MAX_TESS_LEVEL, MIN_TESS_LEVEL, min(distance11, distance10) );
+
+    gl_TessLevelOuter[0] = tessLevel0;
+    gl_TessLevelOuter[1] = tessLevel1;
+    gl_TessLevelOuter[2] = tessLevel2;
+    gl_TessLevelOuter[3] = tessLevel3;
+
+    gl_TessLevelInner[0] = max(tessLevel1, tessLevel3);
+    gl_TessLevelInner[1] = max(tessLevel0, tessLevel2);
+  }
+}
+)";
+const char* TES = R"(
+#version 450 core
+layout(quads, fractional_odd_spacing, ccw) in;
+
+uniform sampler2D heightMap;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+in vec2 TextureCoord[];
+
 out float Height;
-out vec3 Position;
+
+void main()
+{
+    float u = gl_TessCoord.x;
+    float v = gl_TessCoord.y;
+
+    vec2 t00 = TextureCoord[0];
+    vec2 t01 = TextureCoord[1];
+    vec2 t10 = TextureCoord[2];
+    vec2 t11 = TextureCoord[3];
+
+    vec2 t0 = (t01 - t00) * u + t00;
+    vec2 t1 = (t11 - t10) * u + t10;
+    vec2 texCoord = (t1 - t0) * v + t0;
+
+    Height = texture(heightMap, texCoord).y * 64.0 - 16.0;
+
+    vec4 p00 = gl_in[0].gl_Position;
+    vec4 p01 = gl_in[1].gl_Position;
+    vec4 p10 = gl_in[2].gl_Position;
+    vec4 p11 = gl_in[3].gl_Position;
+
+    vec4 uVec = p01 - p00;
+    vec4 vVec = p10 - p00;
+    vec4 normal = normalize( vec4(cross(vVec.xyz, uVec.xyz), 0) );
+
+    vec4 p0 = (p01 - p00) * u + p00;
+    vec4 p1 = (p11 - p10) * u + p10;
+    vec4 p = (p1 - p0) * v + p0 + normal * Height;
+
+    gl_Position = projection * view * model * p;
+}
+)";
+const char* VS = R"(
+#version 450 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec2 aTex;
+
+out vec2 TexCoord;
 
 void main(){
-  Height = aPos.y;
-  Position = (model * vec4(aPos, 1.0)).xyz;
-  gl_Position = projection * view * model * vec4(aPos, 1.0f);
+  gl_Position = vec4(aPos, 1.0f);
+  TexCoord = aTex;
 }
 )";
 
@@ -72,15 +158,6 @@ void main(){
   FragColor = vec4(h, h, h, 1.0);
 }
 )";
-const char* FS2 = R"(
-#version 450 core
-out vec4 FragColor;
-
-void main(){
-  FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-}
-)";
-
 void processInput(GLFWwindow* window)
 {
     float cameraSpeed = 10.0f * deltaTime;
@@ -117,7 +194,6 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     yaw   += xoffset;
     pitch += yoffset;
 
-    // Prevent screen flip
     if (pitch > 89.0f)  pitch = 89.0f;
     if (pitch < -89.0f) pitch = -89.0f;
 
@@ -162,7 +238,8 @@ int main(){
     std::cout << "Failed to initialize GLAD" << std::endl;
     return -1;
   }
-
+  int maxTessLevel;
+  glGetIntegerv(GL_MAX_TESS_GEN_LEVEL, &maxTessLevel);
   glEnable(GL_DEPTH_TEST);
   //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -170,139 +247,104 @@ int main(){
   vertexShader = glCreateShader(GL_VERTEX_SHADER);
   glShaderSource(vertexShader, 1, &VS, NULL);
   glCompileShader(vertexShader);
-  // After glCompileShader(vertexShader)
 
   unsigned int fragmentShader1;
   fragmentShader1 = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader1, 1, &FS1, NULL);
   glCompileShader(fragmentShader1);
-  unsigned int fragmentShader2;
-  fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragmentShader2, 1, &FS2, NULL);
-  glCompileShader(fragmentShader2);
 
+  unsigned int tessControlShader;
+  tessControlShader = glCreateShader(GL_TESS_CONTROL_SHADER);
+  glShaderSource(tessControlShader, 1, &TCS, NULL);
+  glCompileShader(tessControlShader);
+  
+  unsigned int tessEvaluationShader;
+  tessEvaluationShader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+  glShaderSource(tessEvaluationShader, 1, &TES, NULL);
+  glCompileShader(tessEvaluationShader);
+  
   unsigned int shaderProgram1;
   shaderProgram1 = glCreateProgram();
   glAttachShader(shaderProgram1, vertexShader);
   glAttachShader(shaderProgram1, fragmentShader1);
+  glAttachShader(shaderProgram1, tessControlShader);
+  glAttachShader(shaderProgram1, tessEvaluationShader);
   glLinkProgram(shaderProgram1);
-  //unsigned int shaderProgram2;
-  //shaderProgram2 = glCreateProgram();
-  //glAttachShader(shaderProgram2, vertexShader);
-  //glAttachShader(shaderProgram2, fragmentShader2);
-  //glLinkProgram(shaderProgram2);
 
-
-  stbi_set_flip_vertically_on_load(true);
+  glPatchParameteri(GL_PATCH_VERTICES, 4);
+  //stbi_set_flip_vertically_on_load(true);
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture); 
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
   int width, height, nChannels;
   unsigned char *data = stbi_load("src/iceland_heightmap.png", &width, &height, &nChannels, 0);
-  
+  if (data)
+  {
+    GLenum format = (nChannels == 4) ? GL_RGBA : GL_RGB;
+    glTexImage2D(GL_TEXTURE_2D, 0, format,
+                 width, height, 0,
+                 format, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else
+  {
+    std::cout << "Failed to load heightmap\n";
+  }
+
   std::vector<float> vertices;
-  float yScale = 64.0f / 256.0f, yShift = 16.0f;  // apply a scale+shift to the height data
-  for(unsigned int i = 0; i < height; i++)
+
+  unsigned rez = 20;
+  for(unsigned i = 0; i <= rez-1; i++)
   {
-    for(unsigned int j = 0; j < width; j++)
-    {
-        // retrieve texel for (i,j) tex coord
-      unsigned char* texel = data + (j + width * i) * nChannels;
-        // raw height at coordinate
-      unsigned char y = texel[0];
+	  for(unsigned j = 0; j <= rez-1; j++)
+	  {
+		  vertices.push_back(-width/2.0f + width*i/(float)rez); // v.x
+		  vertices.push_back(0.0f); // v.y
+		  vertices.push_back(-height/2.0f + height*j/(float)rez); // v.z
+		  vertices.push_back(i / (float)rez); // u
+		  vertices.push_back(j / (float)rez); // v
 
-        // vertex
-      vertices.push_back( -height/2.0f + height*i/(float)height );        // v.x
-      vertices.push_back( (int)y * yScale - yShift); // v.y
-      vertices.push_back( -width/2.0f + width*j/(float)width );        // v.z
-    }
+		  vertices.push_back(-width/2.0f + width*(i+1)/(float)rez); // v.x
+		  vertices.push_back(0.0f); // v.y
+		  vertices.push_back(-height/2.0f + height*j/(float)rez); // v.z
+		  vertices.push_back((i+1) / (float)rez); // u
+		  vertices.push_back(j / (float)rez); // v
+
+		  vertices.push_back(-width/2.0f + width*i/(float)rez); // v.x
+		  vertices.push_back(0.0f); // v.y
+		  vertices.push_back(-height/2.0f + height*(j+1)/(float)rez); // v.z
+		  vertices.push_back(i / (float)rez); // u
+		  vertices.push_back((j+1) / (float)rez); // v
+
+		  vertices.push_back(-width/2.0f + width*(i+1)/(float)rez); // v.x
+		  vertices.push_back(0.0f); // v.y
+		  vertices.push_back(-height/2.0f + height*(j+1)/(float)rez); // v.z
+		  vertices.push_back((i+1) / (float)rez); // u
+		  vertices.push_back((j+1) / (float)rez); // v
+	  }
   }
-  std::cout << "Loaded " << vertices.size() / 3 << " vertices" << std::endl;
-
   stbi_image_free(data);
-  
-  std::vector<unsigned int> indices;
-  for(unsigned int i = 0; i < height-1; i++)       // for each row a.k.a. each strip
-  {
-    for(unsigned int j = 0; j < width; j++)      // for each column
-    {
-      for(unsigned int k = 0; k < 2; k++)      // for each side of the strip
-      {
-        indices.push_back(j + width * (i + k));
-      }
-    }
-  }
-  std::cout << "Loaded " << indices.size() << " indices" << std::endl;
+  std::cout << "Loaded " << rez*rez << " patches of 4 control points each" << std::endl;
+  std::cout << "Processing " << rez*rez*4 << " vertices in vertex shader" << std::endl;
 
-  
-  /*float cube[] = {
-    -0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    
-    +0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    
-    -0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-
-    -0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-
-    -0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    
-    -0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f,
-    -0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, +0.5f, +0.5f, 1.0f, 0.0f, 0.0f,
-    +0.5f, +0.5f, -0.5f, 1.0f, 0.0f, 0.0f
-  };
-  glm::vec3 cubePositions[] = {
-    glm::vec3( 0.0f,  0.0f,  0.0f),
-    glm::vec3( 2.0f,  5.0f, -15.0f),
-    glm::vec3(-1.5f, -2.2f, -2.5f),
-    glm::vec3(-3.8f, -2.0f, -12.3f),
-    glm::vec3( 2.4f, -0.4f, -3.5f),
-    glm::vec3(-1.7f,  3.0f, -7.5f),
-    glm::vec3( 1.3f, -2.0f, -2.5f),
-    glm::vec3( 1.5f,  2.0f, -2.5f),
-    glm::vec3( 1.5f,  0.2f, -1.5f),
-    glm::vec3(-1.3f,  1.0f, -1.5f)
-  };*/
-  const unsigned int NUM_STRIPS = height-1;
-  const unsigned int NUM_VERTS_PER_STRIP = width*2;
-
-  unsigned int VBO[1], VAO[1], EBO[1];
+  unsigned int VBO[1], VAO[1];
   glGenBuffers(1, VBO);
   glGenVertexArrays(1, VAO);
   glBindVertexArray(VAO[0]);
   glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
   glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), &vertices[0], GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  
-  glGenBuffers(1, EBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), &indices[0], GL_STATIC_DRAW);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),(void*) (3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
+  glPatchParameteri(GL_PATCH_VERTICES, NUM_PATCH_PTS);
+  
 
   while(!glfwWindowShouldClose(w)){
     float currentFrame = glfwGetTime();
@@ -316,42 +358,29 @@ int main(){
     glUseProgram(shaderProgram1);
     
     glm::mat4 view = glm::mat4(1.0f);
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100000.0f);
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 5000.0f);
     view = glm::lookAt(
       cameraPos,
       cameraPos + cameraFront,
       cameraUp
     );
     glm::mat4 model = glm::mat4(1.0f);
-
+    
+    glUniform1i(glGetUniformLocation(shaderProgram1, "heightMap"), 0);
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram1, "model"), 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram1, "view"), 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(glGetUniformLocation(shaderProgram1, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
 
 
     glBindVertexArray(VAO[0]);
-    /*for (unsigned int i = 0; i < 10; i++) {
-      glm::mat4 model = glm::mat4(1.0f);
-      model = glm::translate(model, cubePositions[i]);
-      float angle = 20.0f * i;
-      model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-      glUniformMatrix4fv(glGetUniformLocation(shaderProgram1, "model"), 1, GL_FALSE, glm::value_ptr(model));
-      glDrawArrays(GL_TRIANGLES, 0, 36);
-    }*/
-   // glUseProgram(shaderProgram2);
-    //glBindVertexArray(VAO[1]);
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
-    for (unsigned int strip = 0; strip < NUM_STRIPS; strip++) {
-      glDrawElements(GL_TRIANGLE_STRIP, NUM_VERTS_PER_STRIP, GL_UNSIGNED_INT, (void*)(sizeof(unsigned int) * NUM_VERTS_PER_STRIP * strip));
-    }
+    glDrawArrays(GL_PATCHES, 0, NUM_PATCH_PTS*rez*rez);
+    
     glfwSwapBuffers(w);
     glfwPollEvents();
   }
   glDeleteVertexArrays(1, VAO);
   glDeleteBuffers(1, VBO);
-  glDeleteBuffers(1, EBO);
   glDeleteProgram(shaderProgram1);
-  //glDeleteProgram(shaderProgram2);
 
   glfwTerminate();
   return 0;
